c941bfa1888d8730cef6c43c5ef35627b23f7020
velpulakavitha <kavitha.v@medvol.in>
2024-03-20 19:27:52 +0530

    main nav bar

--- src/navigations/AppNavigation.js
+++ src/navigations/AppNavigation.js
@@ -1,175 +1,6 @@
 
-// import React, { useLayoutEffect } from 'react';
-// import { View, StyleSheet } from 'react-native';
-// import { createStackNavigator } from '@react-navigation/stack';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createDrawerNavigator } from '@react-navigation/drawer';
-// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'; // Import the bottom tab navigator
-// import HomeScreen from '../screens/Home/HomeScreen';
-// import CategoriesScreen from '../screens/Categories/CategoriesScreen';
-// import RecipeScreen from '../screens/Recipe/RecipeScreen';
-// import RecipesListScreen from '../screens/RecipesList/RecipesListScreen';
-// import DrawerContainer from '../screens/DrawerContainer/DrawerContainer';
-// import IngredientScreen from '../screens/Ingredient/IngredientScreen';
-// import SearchScreen from '../screens/Search/SearchScreen';
-// import IngredientsDetailsScreen from '../screens/IngredientsDetails/IngredientsDetailsScreen';
-// import SignIn from '../screens/SignIn/SignIn';
-// import SignUP from '../screens/SignUp/SignUp';
-// import RestPassword from '../screens/ResetPassword/ResetPassword';
-// import ProductDescription from '../components/ProductDescription/ProductDescription';
-// import MyCart from '../components/MyCart/MyCart';
-// import OrderSummary from '../components/OrderSummary/OrderSummary';
-// import OrderSteps from '../components/OrderSummary/OrderSummary';
-// import { FontAwesome, Ionicons } from '@expo/vector-icons';
-// import OTPPage from '../screens/OTPPage/Otppage';
-// import { Profilepage } from '../components/Profile/Profilepage';
-// const Stack = createStackNavigator();
-// const Drawer = createDrawerNavigator();
-// const Tab = createBottomTabNavigator(); // Create a bottom tab navigator
-
-// function MainNavigator() {
-//   return (
-//     <Stack.Navigator
-//       screenOptions={{
-//         headerTitleStyle: {
-//           fontWeight: 'bold',
-//           textAlign: 'center',
-//           alignSelf: 'center',
-//           flex: 1,
-//           marginTop: 12,
-//         }
-//       }}
-//     >
-
-//       <Stack.Screen name='Home' component={HomeScreen} />
-//       <Stack.Screen name='Categories' component={CategoriesScreen} />
-//       <Stack.Screen name='Recipe' component={RecipeScreen} />
-//       <Stack.Screen name='RecipesList' component={RecipesListScreen} />
-//       <Stack.Screen name='Ingredient' component={IngredientScreen} />
-//       <Stack.Screen name='Search' component={SearchScreen} />
-//       <Stack.Screen name='IngredientsDetails' component={IngredientsDetailsScreen} />
-//       <Stack.Screen name='ProductDescription' component={ProductDescription} />
-//       <Stack.Screen name='MyCart' component={MyCart} />
-//       <Stack.Screen name='OrderSummary' component={OrderSteps} />
-//       <Stack.Screen name='ProfileEdit' component={Profilepage} />
-//     </Stack.Navigator>
-//   )
-// }
-
-// function SignInNavigator() {
-//   return (
-//     <Stack.Navigator>
-//       <Stack.Screen name="SignIn" component={SignIn} />
-//       <Stack.Screen name="SignUp" component={SignUP} />
-//       <Stack.Screen name="ResetPassword" component={RestPassword} />
-//       <Stack.Screen name='OTP' component={OTPPage} />
-//     </Stack.Navigator>
-//   );
-// }
-
-// function DrawerStack() {
-//   return (
-//     <Drawer.Navigator
-//       drawerPosition='left'
-//       initialRouteName='Main'
-//       drawerStyle={{
-//         width: 250
-//       }}
-//       screenOptions={{ headerShown: false }}
-//       drawerContent={({ navigation }) => <DrawerContainer navigation={navigation} />}
-//     >
-//       <Drawer.Screen name='Main' component={MainNavigator} />
-//     </Drawer.Navigator>
-//   )
-// }
-
-
-// function BottomTabNavigator({ navigation }) {
-//   return (
-//     <Tab.Navigator
-//       screenOptions={{ headerShown: false }}
-//       tabBarOptions={{
-//         activeTintColor: 'green', // Set the active color to green
-//         labelStyle: {
-//           fontSize: 14, // Adjust font size as needed
-//           paddingBottom: 3, // Adjust padding bottom as needed
-//         },
-//         style: {
-//           height: 100, // Adjust height of the tab bar
-//         },
-//       }}
-//     >
-//       <Tab.Screen
-//         name="Home"
-//         component={DrawerStack}
-//         options={{
-//           tabBarLabel: 'Home',
-//           tabBarIcon: ({ color, size }) => (
-//             <Ionicons name="home" color={color} size={size} />
-//           ),
-//         }}
-//       />
-//       <Tab.Screen
-//         name="MyCart"
-//         component={MyCart}
-//         options={{
-//           tabBarLabel: 'My Cart',
-//           tabBarIcon: ({ color, size }) => (
-//             <FontAwesome name="shopping-cart" color={color} size={size} />
-//           ),
-//           onPress: () => navigation.navigate('MyCart'), // Navigate to MyCart screen when tab is pressed
-//         }}
-//       />
-//       <Tab.Screen
-//         name="OrderSummary"
-//         component={OrderSummary}
-//         options={{
-//           tabBarLabel: 'My Orders',
-//           tabBarIcon: ({ color, size }) => (
-//             <FontAwesome name="list-alt" color={color} size={size} />
-//           ),
-//           onPress: () => navigation.navigate('OrderSummary'), // Navigate to OrderSummary screen when tab is pressed
-//         }}
-//       />
-//       <Tab.Screen
-//         name="ProfileEdit"
-//         component={Profilepage}
-//         options={{
-//           tabBarLabel: 'Profile Edit',
-//           tabBarIcon: ({ color, size }) => (
-//             <FontAwesome name="user" color={color} size={size} />
-//           ),
-//           onPress: () => navigation.navigate('ProfileEdit'), // Navigate to OrderSummary screen when tab is pressed
-//         }}
-//       />
-//     </Tab.Navigator>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     height: 500,
-//     paddingBottom: 0, // Adjust padding bottom as needed
-//   },
-// });
-
-
-// export default function AppContainer() {
-//   return (
-//     <NavigationContainer>
-//       <Stack.Navigator headerMode="none">
-//         <Stack.Screen name="SignIn" component={SignInNavigator} />
-//         <Stack.Screen name="Home" component={BottomTabNavigator} />
-
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   )
-// }
-
-// console.disableYellowBox = true;
-
 import React from 'react';
+import {View} from 'react-native'
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -189,7 +20,7 @@ import OrderSummary from '../components/OrderSummary/OrderSummary';
 import OrderSteps from '../components/OrderSummary/OrderSummary';
 import { FontAwesome, Ionicons } from '@expo/vector-icons';
 import OTPPage from '../screens/OTPPage/Otppage';
-import ProfileEdit, { Profilepage } from '../components/Profile/Profilepage';
+import ProfileEdit from '../components/Profile/Profilepage';
 import ProfilePage from '../components/Profile/Profile';
 
 const Stack = createStackNavigator();
@@ -218,7 +49,8 @@ function MainNavigator() {
       <Stack.Screen name='ProductDescription' component={ProductDescription} />
       <Stack.Screen name='MyCart' component={MyCart} />
       <Stack.Screen name='OrderSummary' component={OrderSteps} />
-      <Stack.Screen name='ProfileEdit' component={Profilepage} />
+      <Stack.Screen name='ProfileEdit' component={ProfileEdit} />
+      <Stack.Screen name='ProfilePage' component={ProfilePage} />
     </Stack.Navigator>
   )
 }
@@ -260,7 +92,7 @@ function MainNavigator_OrderSummary() {
 }
 
 
-function MainNavigator_ProfileEdit() {
+function MainNavigator_Profile() {
   return (
     <Stack.Navigator
       screenOptions={{
@@ -273,7 +105,7 @@ function MainNavigator_ProfileEdit() {
         }
       }}
     >
-      <Stack.Screen name='ProfileEdit' component={Profilepage} />
+      <Stack.Screen name='ProfilePage' component={ProfilePage} />
     </Stack.Navigator>
   )
 }
@@ -296,7 +128,11 @@ function BottomTabNavigator() {
         activeTintColor: '#347855',
         labelStyle: {
           fontSize: 14,
-          paddingBottom: 3,
+          paddingTop: 0,
+        },
+        tabBarStyle: {
+          display: 'flex',
+          paddingTop: 15, // Adjust this value to set the padding for the top of the tab bar
         },
         style: {
           height: 100,
@@ -307,10 +143,11 @@ function BottomTabNavigator() {
         name="Home"
         component={MainNavigator}
         options={{
-
-          tabBarLabel: 'Home',
+          tabBarLabel: '',
           tabBarIcon: ({ color, size }) => (
-            <Ionicons name="home" color={color} size={size} />
+            <View style={{ paddingTop: 7 }}>
+              <Ionicons name="home" color={color} size={size} />
+            </View>
           ),
         }}
       />
@@ -318,9 +155,11 @@ function BottomTabNavigator() {
         name="MyCart"
         component={MainNavigator_MYcart}
         options={{
-          tabBarLabel: 'My Cart',
+          tabBarLabel: '',
           tabBarIcon: ({ color, size }) => (
-            <FontAwesome name="shopping-cart" color={color} size={size} />
+            <View style={{ paddingTop: 7 }}>
+              <FontAwesome name="shopping-cart" color={color} size={size} />
+            </View>
           ),
         }}
       />
@@ -328,26 +167,30 @@ function BottomTabNavigator() {
         name="OrderSummary"
         component={MainNavigator_OrderSummary}
         options={{
-          tabBarLabel: 'My Orders',
+          tabBarLabel: '',
           tabBarIcon: ({ color, size }) => (
-            <FontAwesome name="list-alt" color={color} size={size} />
+            <View style={{ paddingTop: 7 }}>
+              <FontAwesome name="list-alt" color={color} size={size} />
+            </View>
           ),
         }}
       />
       <Tab.Screen
-        name="ProfileEdit"
-        component={MainNavigator_ProfileEdit}
+        name="ProfilePage"
+        component={MainNavigator_Profile}
         options={{
-          tabBarLabel: 'Profile Edit',
+          tabBarLabel: '',
           tabBarIcon: ({ color, size }) => (
-            <FontAwesome name="user" color={color} size={size} />
+            <View style={{ paddingTop: 7 }}>
+              <FontAwesome name="user" color={color} size={size} />
+            </View>
           ),
-          onPress: () => navigation.navigate('ProfilePage'), // Navigate to OrderSummary screen when tab is pressed
         }}
       />
     </Tab.Navigator>
   );
 }
+
 
 export default function AppContainer() {
   return (
